e1cc2877ad200d6101882af237abe8827db08e03
Shakir <shakir@example.com>
2025-11-20 02:02:22 -0800

    Task

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,26 @@
 import Heading from "./pages/heading";
 
 export default function Home() {
+  const user = [
+    {
+      id:1,
+      username:"Ali",
+      postion:"frontend",
+      salary:"20k"
+    },
+    {
+      id:2,
+      username:"khan",
+      postion:"Ai developer!",
+      salary:"50k"
+    },
+    {
+      id:2,
+      username:"fawad",
+      postion:"full-stack",
+      salary:"60k"
+    }
+  ]
   return (
     <>
       <Heading />
