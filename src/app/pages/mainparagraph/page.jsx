59abf7c2a71e6a052d46c50e20da644e1c38534d
Itx-Ahmed <ahmadiqbal858687@gmail.com>
2025-11-20 03:51:15 -0800

    mainparagraph created

---
+++ src/app/pages/mainparagraph/page.jsx
@@ -0,0 +1,14 @@
+const MainParagraph = () => {
+  return (
+    <>
+      <h1 className="bg-black text-red">Main paragarph is here</h1>
+      <p className="bg-black text-white">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+        laboriosam praesentium ducimus. Ratione distinctio, iure esse nesciunt
+        voluptas molestiae voluptate.
+      </p>
+    </>
+  );
+};
+
+export default MainParagraph;
